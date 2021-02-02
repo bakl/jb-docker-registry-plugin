ffcc1a6dee140dcc56c76511cd90c78bbf34a304
GitHub Action <action@github.com>
2021-02-02 15:35:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/bakl/jbdockerregistryplugin/MyBundle.kt
+++ src/main/kotlin/com/github/bakl/jbdockerregistryplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.bakl.jbdockerregistryplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/bakl/jbdockerregistryplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.bakl.jbdockerregistryplugin.services
+
+import com.github.bakl.jbdockerregistryplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
